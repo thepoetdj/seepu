191c2b78a1131100ac642625bbf4d920fec7e31b
Dhruv Joshi <joshidhruv997@gmail.com>
2024-04-12 21:21:27 +0530

    removed unnecessary flags from stats collection command and simplified JSON output

--- stats.sh
+++ stats.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-STATS_TEMP_FILE="cpu-stats"
+STATS_TEMP_FILE=/tmp/seepu.stats
 # fetch memory usage statistics
-STATS=$(sar -r 1 1 -o $STATS_TEMP_FILE &> 1 && sadf $STATS_TEMP_FILE -j --iface=eth1 -- -u -r -n DEV | jq -c '.sysstat')
+STATS=$(sar -r 1 1 -o $STATS_TEMP_FILE &> 1 && sadf $STATS_TEMP_FILE -j -- -u -r | jq -c '.sysstat')
 # remove temporary file
 rm -f $STATS_TEMP_FILE
-
 # output formatted JSON for consumption
-echo '{"topic":"'"${TOPIC:-cpu-stats}"'","event":'"$STATS"'}' >> ${STATS_DIR:-/var/log}/seepu.json
+echo $STATS >> ${STATS_DIR:-/var/log}/seepu.log
